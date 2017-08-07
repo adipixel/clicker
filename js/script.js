267bb3572905a35511dc2b548564c7de7b6a63ab
adipixel <me@adityamhamunkar.com>
2017-08-07 19:10:16 -0400

    Added a list of dog names and on clicking one, displays information about the clicked one

--- js/script.js
+++ js/script.js
@@ -5,6 +5,38 @@ $( document ).ready(function() {
 	var dogs = [{name: 'Bunty', img_url:'images/bunty.jpg', count:0}, {name: 'Mark', img_url:'images/mark.jpg', count:0}];
 	
 	for (var i = 0; i < dogs.length; i++) {
+		var $list_item = $('<li class="dog_li">'+dogs[i].name+'</li>');
+		$('#dog-list').append($list_item);
+
+		$list_item.on('click',(function(i){
+			return function(){
+				$dogImgHolder.text('');
+				var $dogclicker = $('<div class="col-sm-6"></div>');
+				var $counter = $('<label class="counter" id="counter'+i+'">'+dogs[i].count+'</label>');
+				var dogImg = '<label id="dog-name">'+dogs[i].name+'</label><img class="text-center img-clicker" id="img-clicker" src="'+dogs[i].img_url+'" width="500">'
+				
+
+				$dogclicker.on('click',(function(i){
+					return function(){
+						dogs[i].count++;
+						$('#counter'+i+'').text(dogs[i].count);
+					}
+				})(i));
+
+				$dogclicker.append($counter);
+				$dogclicker.append(dogImg);
+				$dogImgHolder.append($dogclicker);
+
+
+			}
+		})(i));
+
+	}
+
+
+
+/*
+	for (var i = 0; i < dogs.length; i++) {
 
 		var $dogclicker = $('<div class="col-sm-6"></div>');
 		var $counter = $('<label class="counter" id="counter'+i+'">'+dogs[i].count+'</label>');
@@ -23,7 +55,7 @@ $( document ).ready(function() {
 		$dogImgHolder.append($dogclicker);
 
 	}
-	
+	*/
 
 
 	
