e2f55c3690dbec841f95e10e76ebd40c956df60d
adipixel <me@adityamhamunkar.com>
2017-08-07 18:28:17 -0400

    Added magic code for  to use a for loop to iterate over the list elements, and when the click happens, alert the value of num as we iterate over each item of the array

--- js/script.js
+++ js/script.js
@@ -2,25 +2,29 @@
 $( document ).ready(function() {
 
 	var $dogImgHolder = $('#dog-img-container');
-	var dogs = [{name: 'Bunty', img_url:'images/bunty.jpg'}, {name: 'Mark', img_url:'images/mark.jpg'}];
+	var dogs = [{name: 'Bunty', img_url:'images/bunty.jpg', count:0}, {name: 'Mark', img_url:'images/mark.jpg', count:0}];
 	
 	for (var i = 0; i < dogs.length; i++) {
-		var dogImg = '<div class="col-sm-6"><label id="dog-name">'+dogs[i].name+'</label><img class="text-center img-clicker" id="img-clicker" src="'+dogs[i].img_url+'" width="500"></div>'
-		$dogImgHolder.append(dogImg);
-		console.log(dogImg);
+
+		var $dogclicker = $('<div class="col-sm-6"></div>');
+		var $counter = $('<label class="counter" id="counter'+i+'">'+dogs[i].count+'</label>');
+		var dogImg = '<label id="dog-name">'+dogs[i].name+'</label><img class="text-center img-clicker" id="img-clicker" src="'+dogs[i].img_url+'" width="500">'
+		
+
+		$dogclicker.on('click',(function(i){
+			return function(){
+				dogs[i].count++;
+				$('#counter'+i+'').text(dogs[i].count);
+			}
+		})(i));
+
+		$dogclicker.append($counter);
+		$dogclicker.append(dogImg);
+		$dogImgHolder.append($dogclicker);
+
 	}
 	
-	var $imgClicker = $('.img-clicker');
-	var $count = $('#count');
 
-
-
-	$imgClicker.on('click', function(){
-		var count = parseInt($count.text());
-		$count.text('');
-		count++;
-		$count.text(count);
-	});
 
 	
 
