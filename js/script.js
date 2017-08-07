b4a81531b43c774e7ff230875bfcb456e477a052
adipixel <me@adityamhamunkar.com>
2017-08-07 17:05:36 -0400

    According to new requirements, two dog images are added. For future convinience, the images, and their names are appended from javascript

--- js/script.js
+++ js/script.js
@@ -1,8 +1,27 @@
-var $imgClicker = $('#img-clicker');
-var $count = $('#count');
-$imgClicker.on('click', function(){
-	var count = parseInt($count.text());
-	$count.text('');
-	count++;
-	$count.text(count);
+
+$( document ).ready(function() {
+
+	var $dogImgHolder = $('#dog-img-container');
+	var dogs = [{name: 'Bunty', img_url:'images/bunty.jpg'}, {name: 'Mark', img_url:'images/mark.jpg'}];
+	
+	for (var i = 0; i < dogs.length; i++) {
+		var dogImg = '<div class="col-sm-6"><label id="dog-name">'+dogs[i].name+'</label><img class="text-center img-clicker" id="img-clicker" src="'+dogs[i].img_url+'" width="500"></div>'
+		$dogImgHolder.append(dogImg);
+		console.log(dogImg);
+	}
+	
+	var $imgClicker = $('.img-clicker');
+	var $count = $('#count');
+
+
+
+	$imgClicker.on('click', function(){
+		var count = parseInt($count.text());
+		$count.text('');
+		count++;
+		$count.text(count);
+	});
+
+	
+
 });
